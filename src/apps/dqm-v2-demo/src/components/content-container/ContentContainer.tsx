2348f887410e73a70a9b4e8789e8947a3218d747
Utku Sarioglu <utkusarioglu@hotmail.com>
2025-12-08 14:56:29 +0000

    Fix: ContentDisplay error height

--- src/apps/dqm-v2-demo/src/components/content-container/ContentContainer.tsx
+++ src/apps/dqm-v2-demo/src/components/content-container/ContentContainer.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "@tanstack/react-router";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "../error-fallback/ErrorFallback";
+import { Scroller } from "../scroller/Scroller";
 
 export const ContentContainer = () => {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Outlet />
+      <Scroller direction="vertical">
+        <Outlet />
+      </Scroller>
     </ErrorBoundary>
   );
 };
